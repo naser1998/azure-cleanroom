b2602928bd436366487636f1380d0a3692500955
Naser <naser.omari@outlook.com>
2026-03-26 13:59:36 +0100

    Example of working cleanroom

---
+++ src/internal/restapi-common/Certs/AutoRenewingCertHandler.cs
@@ -0,0 +1,70 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Security.Authentication;
+using Microsoft.Extensions.Logging;
+
+namespace Controllers;
+
+public class AutoRenewingCertHandler : DelegatingHandler
+{
+    private readonly ILogger logger;
+    private readonly ServiceCertLocator certLocator;
+    private readonly Action<string> onRenewal;
+
+    public AutoRenewingCertHandler(
+        ILogger logger,
+        ServiceCertLocator certLocator,
+        ServerCertValidationHandler serverCertValidationHandler,
+        Action<string> onRenewal)
+        : base(serverCertValidationHandler)
+    {
+        this.logger = logger;
+        this.certLocator = certLocator;
+        this.onRenewal = onRenewal;
+    }
+
+    protected override async Task<HttpResponseMessage> SendAsync(
+        HttpRequestMessage request,
+        CancellationToken cancellationToken)
+    {
+        const string RetryKey = "CertRefreshRetry";
+
+        if (!request.Options.TryGetValue(
+            new HttpRequestOptionsKey<bool>(RetryKey),
+            out var hasRetried))
+        {
+            hasRetried = false;
+        }
+
+        try
+        {
+            return await base.SendAsync(request, cancellationToken);
+        }
+        catch (HttpRequestException ex)
+        when (!hasRetried && ex.InnerException is AuthenticationException)
+        {
+            this.logger.LogWarning($"SSL validation failed during " +
+                $"{request.Method} {request.RequestUri} — redownloading certificate.");
+
+            string serviceCertPem = await this.certLocator.DownloadServiceCertificatePem();
+
+            this.onRenewal(serviceCertPem);
+
+            // Update the SSL cert being used by the InnerHandler for validation.
+            if (this.InnerHandler is not ServerCertValidationHandler certValidationHandler)
+            {
+                throw new Exception(
+                    $"InnerHandler was expected of type ServerCertValidationHandler but is " +
+                    $"{this.InnerHandler?.GetType().ToString()}");
+            }
+
+            certValidationHandler.UpdateServiceCert(serviceCertPem);
+
+            // Set the retry flag so we don't retry again if the same failure happens again.
+            request.Options.Set(new HttpRequestOptionsKey<bool>(RetryKey), true);
+
+            return await base.SendAsync(request, cancellationToken); // Retry once.
+        }
+    }
+}
---
+++ src/internal/restapi-common/Certs/ServerCertValidationHandler.cs
@@ -0,0 +1,140 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
+using Microsoft.Extensions.Logging;
+
+namespace Controllers;
+
+public class ServerCertValidationHandler : DelegatingHandler
+{
+    private List<string>? serviceCertPems;
+    private ILogger logger;
+    private X509Certificate2Collection? roots;
+
+    public ServerCertValidationHandler(
+        ILogger logger,
+        string? serviceCertPem,
+        bool skipTlsVerify = false,
+        X509Certificate2? clientCert = null,
+        string? endpointName = null)
+        : this(
+            logger,
+            serviceCertPem == null ? [] : [serviceCertPem],
+            skipTlsVerify,
+            clientCert,
+            endpointName)
+    {
+    }
+
+    public ServerCertValidationHandler(
+        ILogger logger,
+        List<string> serviceCertPems,
+        bool skipTlsVerify = false,
+        X509Certificate2? clientCert = null,
+        string? endpointName = null)
+    {
+        this.logger = logger;
+        endpointName ??= "endpoint";
+        this.SetRootCertsCollection(logger, serviceCertPems);
+        var socketsHandler = new SocketsHttpHandler
+        {
+            // Avoid DNS refresh issues for long lived clients.
+            // https://www.stevejgordon.co.uk/httpclient-connection-pooling-in-dotnet-core
+            PooledConnectionLifetime = TimeSpan.FromMinutes(1),
+            SslOptions =
+            {
+                ClientCertificates = clientCert != null ?
+                    new X509Certificate2Collection(clientCert) : null,
+                RemoteCertificateValidationCallback = (request, cert1, chain, errors) =>
+                {
+                    if (errors == SslPolicyErrors.None)
+                    {
+                        return true;
+                    }
+
+                    if (cert1 == null || chain == null)
+                    {
+                        return false;
+                    }
+
+                    if (this.roots == null)
+                    {
+                        if (skipTlsVerify)
+                        {
+                            return true;
+                        }
+
+                        logger.LogError(
+                            "Failing SSL cert validation callback as no SSL cert to use for " +
+                            $"verification was specified.");
+                        return false;
+                    }
+
+                    X509Certificate2 cert = (X509Certificate2)cert1;
+                    foreach (X509ChainElement element in chain.ChainElements)
+                    {
+                        chain.ChainPolicy.ExtraStore.Add(element.Certificate);
+                    }
+
+                    chain.ChainPolicy.CustomTrustStore.Clear();
+                    chain.ChainPolicy.TrustMode = X509ChainTrustMode.CustomRootTrust;
+                    chain.ChainPolicy.CustomTrustStore.AddRange(this.roots);
+                    var result = chain.Build(cert);
+                    if (!result)
+                    {
+                        logger.LogError(
+                            $"{endpointName}: Failing SSL cert validation callback for " +
+                            $"as chain.Build() returned false.");
+                        for (int index = 0; index < chain.ChainStatus.Length; index++)
+                        {
+                            logger.LogError($"{endpointName}: chainStatus[{index}]: " +
+                                $"{chain.ChainStatus[0].Status}, " +
+                                $"{chain.ChainStatus[0].StatusInformation}");
+                        }
+
+                        logger.LogError(
+                            $"{endpointName}: Incoming cert PEM: {cert.ExportCertificatePem()}");
+                        logger.LogError(
+                            $"Expected cert PEMs are: " +
+                            $"{JsonSerializer.Serialize(this.serviceCertPems)}");
+                    }
+
+                    return result;
+                }
+            }
+        };
+        this.InnerHandler = socketsHandler;
+    }
+
+    public void UpdateServiceCert(string serviceCertPem)
+    {
+        this.SetRootCertsCollection(this.logger, [serviceCertPem]);
+    }
+
+    private void SetRootCertsCollection(ILogger logger, List<string> serviceCertPems)
+    {
+        X509Certificate2Collection? roots = null;
+        if (serviceCertPems.Any())
+        {
+            try
+            {
+                roots = new X509Certificate2Collection();
+                foreach (var certPem in serviceCertPems)
+                {
+                    roots.Add(X509Certificate2.CreateFromPem(certPem));
+                }
+            }
+            catch (Exception e)
+            {
+                this.logger.LogError(e, "Unexpected failure in loading cert PEM.");
+                throw;
+            }
+        }
+
+        this.serviceCertPems = serviceCertPems;
+        this.roots = roots;
+    }
+}
